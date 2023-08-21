3582b49b55082e386c1efdb9a31d2719dc84f600
Denys Sych <denys.sych@risktec.io>
2023-08-21 20:39:02 +0200

    moved lib from aqeum-web to core-web

--- apps/aqeum-web/src/app/app.tsx
+++ apps/aqeum-web/src/app/app.tsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import { AuthPanel } from '@dsych-org/aqeum-web/auth';
+import { AuthPanel } from '@dsych-org/core-web/auth';
 
 export function App() {
   return <AuthPanel />;
--- libs/core-web/auth/jest.config.ts
+++ libs/core-web/auth/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'aqeum-web-auth',
+  displayName: 'core-web-auth',
   preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/aqeum-web/auth',
+  coverageDirectory: '../../../coverage/libs/core-web/auth',
 };
